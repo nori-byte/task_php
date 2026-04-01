cb872904dd908644dfd987aa4cadfd545495cf5e
gr441_koaal <kulmanakovap@gmail.com>
2026-04-01 09:35:47 +0700

    error

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gr441_koaal/pop-it-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8e16b377e8955fa94fdf4b1e2ba9b1433a15f5cf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gr441_koaal/pop-it-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8e16b377e8955fa94fdf4b1e2ba9b1433a15f5cf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
